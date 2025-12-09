fbdfa4f638038106e8d1381b3d1f890b73014d8e
Tojammel Hoque <tojammelhoque375@gmail.com>
2025-12-09 18:42:54 +0530

    fix pre hook in  usermodel

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -65,17 +65,15 @@ const userSchema = new Schema<IUser>(
 );
 
 // Hash password before saving
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
 
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-
-  next();
 });
 
 // Compare passwords
-userSchema.methods.comparePassword = function (
+userSchema.methods.comparePassword = async function (
   candidatePassword: string
 ): Promise<boolean> {
   return bcrypt.compare(candidatePassword, this.password);
